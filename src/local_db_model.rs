7e25d954150b04d6d6453bbc903ee21dde595abf
Jhonacode <jhonacode2020@gmail.com>
2025-02-18 14:00:41 +1300

    Covered more test cases and new C implementations, clean and remove database.

--- src/local_db_model.rs
+++ src/local_db_model.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Value as JsonValue;
-#[derive(Deserialize,Serialize,Debug)]
+#[derive(Debug,Deserialize,Serialize, Clone)]
 pub struct LocalDbModel{
     pub id: String,
     pub hash: String,
